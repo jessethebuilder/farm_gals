6e783a64bdd4d408ac106ccf6055e7f6b1bc2d79
Jesse Farmer <jessethebuilder@yahoo.com>
2016-03-20 02:16:47 -0700

    s

--- app/assets/javascripts/farm_gals/all.js
+++ app/assets/javascripts/farm_gals/all.js
@@ -1,11 +1,3 @@
 
 //= require farm_gals/bootstrap_form.js
-
-// function activateSectionCloser(box_selector, toggle_selector, speed){
-	// speed = typeof speed !== 'undefined' ? speed : 400;
-	// $(toggle_selector).click(function(e){
-		// e.preventDefault();
-		// $(box_selector).hide(400);
-// 		
-	// });
-// }
+//= require farm_gals/carousel.js
