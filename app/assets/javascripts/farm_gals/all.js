534b6205eb02ff943868c7b41f6fca3a4623949b
Jesse Farmer <jessethebuilder@yahoo.com>
2017-01-13 14:28:32 -0800

    remove sortable

--- app/assets/javascripts/farm_gals/all.js
+++ app/assets/javascripts/farm_gals/all.js
@@ -1,4 +1,4 @@
 
 //= require farm_gals/bootstrap_form.js
 //= require farm_gals/carousel.js
-//= require farm_gals/ajax_uploader.js
+// require farm_gals/ajax_uploader.js
--- app/assets/javascripts/farm_gals/bootstrap_form.js
+++ app/assets/javascripts/farm_gals/bootstrap_form.js
@@ -1,11 +1,11 @@
-//= require farm_shed/all
-//= require jquery-ui/sortable
+// require farm_shed/all
+// require jquery-ui/sortable
 
 function showUploadFromComputerGroup(group){
 	$(group).find('.upload_from_computer_link').click(function(e){
 		e.preventDefault();
 		$(this).closest('.upload_from_url_group').hide(0, function(){
-			$(this).closest('.gal_image_row').find('.upload_from_computer_group').show(200);	
+			$(this).closest('.gal_image_row').find('.upload_from_computer_group').show(200);
 		});
 	});
 }
@@ -21,7 +21,7 @@ function showUploadFromUrlGroup(group){
 
 function toggleUploadGroups(){
 	var g = $('.gal_image_row');
-	
+
 	for(var i = 0; i < g.length; i++){
 		toggleUploadGroup(g[i], 'computer');
 	}
@@ -30,12 +30,12 @@ function toggleUploadGroups(){
 function toggleUploadGroup(group, initial_state){
 	if(initial_state === 'computer'){
 		$(group).find('.upload_from_url_group').hide();
-		$(group).find('.upload_from_computer_group').show();		
+		$(group).find('.upload_from_computer_group').show();
 	} else if(initial_state === 'url'){
 		$('.upload_from_computer_group').hide();
 		$('.upload_from_url_group').show();
 	} else {
-		throw 'initial_state must be "computer" or "url". Not: ' + initial_state;	
+		throw 'initial_state must be "computer" or "url". Not: ' + initial_state;
 	}
 	showUploadFromUrlGroup(group);
 	showUploadFromComputerGroup(group);
@@ -47,31 +47,31 @@ function _addInvoiceItemRow(){
 	var index = new Date().getTime();
 	row.html(row.html().replace(/_gal_attributes_gal_images_attributes_\d+_/g, '_gal_attributes_gal_images_attributes_' + index + '_'));
 	row.html(row.html().replace(/\[gal_attributes\]\[gal_images_attributes\]\[\d+\]/g, '[gal_attributes][gal_images_attributes][' + index + ']'));
-		
-	$(row).insertAfter(last_row);	
+
+	$(row).insertAfter(last_row);
 	addInvoiceItemRowOnChange(row);
-	return row;	
+	return row;
 }
 
 function addInvoiceItemRowOnChange(row){
 	$(row).find('.upload_field').change(function(){
-		
+
 		if($(this).val() != false){
 			var row = _addInvoiceItemRow();
-			
+
 			// need to refresh this behavior
-			toggleUploadGroup($(row), "computer");	
+			toggleUploadGroup($(row), "computer");
 			resetGalImageOrder();
 		}
-			
+
 	});
 }
 
 function addInvoiceItemRows(){
 	var rows = $('.gal_image_row');
-	
+
 	for(var i = 0; i < rows.length; i++){
-		addInvoiceItemRowOnChange(rows[i]);		
+		addInvoiceItemRowOnChange(rows[i]);
 	}
 }
 
